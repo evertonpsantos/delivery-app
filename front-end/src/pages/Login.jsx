b12def4d1bd237a83d243f2c073da690398b88fd
evertonpsantos <evertonpsantos8@gmail.com>
2023-02-27 19:50:34 -0300

    feat: adiciona elementos na tela

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,7 +1,35 @@
 function Login() {
   return (
     <div>
-      <p>Login</p>
+      <form>
+        <label htmlFor="login-email">
+          Login
+          <input
+            name="login-email"
+            data-testid="common_login__input-email"
+            placeholder="E-mail"
+            type="email"
+          />
+        </label>
+
+        <label htmlFor="login-senha">
+          Senha
+          <input
+            name="login-senha"
+            data-testid="common_login__input-password"
+            placeholder="Senha"
+            type="password"
+          />
+        </label>
+
+        <button type="button" data-testid="common_login__button-login">Login</button>
+
+        <button type="button" data-testid="common_login__button-register">
+          Ainda não tenho conta
+        </button>
+
+        <p data-testid="common_login__element-invalid-email">E-mail inválido</p>
+      </form>
     </div>
   );
 }
