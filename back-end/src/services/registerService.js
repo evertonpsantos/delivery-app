6cbaa977735feaccc12365325e189ffa2183bd7d
Edilson Lima <edilson.lima1@hotmail.com>
2023-03-01 16:30:36 -0300

    Implementando parte do 13, dando erro no Token.

--- back-end/src/services/registerService.js
+++ back-end/src/services/registerService.js
@@ -1,5 +1,6 @@
 const md5 = require('md5');
 const { User } = require('../database/models/index');
+const { newToken } = require('../utils/jwtFunction');
 
 async function registerUser(dataNewUser) {
     const { name, email, password } = dataNewUser;
@@ -13,10 +14,16 @@ async function registerUser(dataNewUser) {
       const findUserEmail = await User.findOne({ where: { email } });
       if (findUserEmail) throw new Error('This email is already registered');
 
+     
+
       const createUser = await User.create({ name, email, password: md, role: 'customer' });
       if (!createUser) throw new Error('Not possible to register this user');
 
-      return createUser;
+      const dadosToken = {name, email, role: 'customer'}
+      const token = await newToken(dadosToken)
+      const userNewToken = { name, email, role: 'customer', token }
+
+      return userNewToken;
     } catch (error) {
       return { message: error.message };
     }
