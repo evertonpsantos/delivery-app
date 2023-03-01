9666c27a7471e20d2bc5b719344726f9a6126c1a
evertonpsantos <evertonpsantos8@gmail.com>
2023-03-01 20:25:41 -0300

    fix: arrumando lint

--- back-end/src/controllers/UserController.js
+++ back-end/src/controllers/UserController.js
@@ -6,7 +6,7 @@ const login = async (req, res) => {
     const result = await loginService(req.body);
     const token = await newToken(result);
     const { id, name, email, role } = result;
-    return res.status(200).json({id, name, email, role, token });
+    return res.status(200).json({ id, name, email, role, token });
   } catch (error) {
     return res.status(404).json(error.message);
   }
--- back-end/src/utils/jwtFunction.js
+++ back-end/src/utils/jwtFunction.js
@@ -5,15 +5,15 @@ require('dotenv').config();
 
 const config = {
   algorithm: 'HS256',
-  noTimestamp: true
+  noTimestamp: true,
 };
 
 const secretPath = path.resolve(__dirname, '..', '..', 'jwt.evaluation.key');
 
-const newToken = async ({name, email, role}) => {
+const newToken = async ({ name, email, role }) => {
   const secret = (await fs.readFile(secretPath, 'utf-8')).trim();
   console.log(secret);
-  return jwt.sign({name, email, role}, secret, config);
+  return jwt.sign({ name, email, role }, secret, config);
 };
 
 const verifyToken = async (token) => {
