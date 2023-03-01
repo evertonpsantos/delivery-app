3342fa2db43eeaa046ba0f9e08f658185f79f4a5
evertonpsantos <evertonpsantos8@gmail.com>
2023-03-01 13:07:22 -0300

    feat: cria a rota para checkout e a page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import LoginProvider from './context/LoginContextProvider';
 import Login from './pages/Login';
+import Checkout from './pages/Checkout';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Redirect to="/login" />
         </Route>
         <Route exact path="/login" component={ Login } />
+        <Route exact path="/customer/checkout" component={ Checkout } />
       </Switch>
     </LoginProvider>
   );
