4887fe88e6386b74c7b795af79cfbf3e003c4b6d
evertonpsantos <evertonpsantos8@gmail.com>
2023-03-03 17:53:44 -0300

    feat: adiciona caminho de imagens em app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,14 +4,14 @@ const registerRouter = require('../routes/register.routes');
 const loginRouter = require('../routes/LoginRouter');
 const productRouter = require('../routes/ProductRouter');
 const salesRouter = require('../routes/SaleRouter');
+const path = require('path');
 
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-// app.use(express.static('public')); 
-// app.use('/images', express.static(path.join(__dirname, '../../images')));
+app.use('/images', express.static(path.resolve(__dirname, '../../images')));
 
 
 app.get('/coffee', (_req, res) => res.status(418).end());
@@ -19,8 +19,5 @@ app.use('/login', loginRouter);
 app.use('/products', productRouter);
 app.use('/register', registerRouter);
 app.use('/sales', salesRouter);
-
-
-// app.use('/images', express.static(`${__dirname}/../../public`));
 
 module.exports = app;
