70888e7d46cf17c31092a63949d48a87cf994fb3
evertonpsantos <evertonpsantos8@gmail.com>
2023-03-03 19:52:51 -0300

    chore: limpando código

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,11 +1,8 @@
-import React, { useContext } from 'react';
-import context from '../context/ProductContext';
+import React from 'react';
 import NavBar from '../components/NavBar';
 import ProductList from '../components/ProductList';
 
 function Products() {
-  const { products } = useContext(context);
-  console.log(products);
   return (
     <>
       <NavBar />
