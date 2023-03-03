4f56fd05cd7ba1981326803e215537f83df9bfeb
evertonpsantos <evertonpsantos8@gmail.com>
2023-03-02 21:34:38 -0300

    feat: adiciona rota para '/sales'

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,9 +3,9 @@ const cors = require('cors');
 const registerRouter = require('../routes/register.routes');
 const loginRouter = require('../routes/LoginRouter');
 const productRouter = require('../routes/ProductRouter');
+const salesRouter = require('../routes/SaleRouter');
 
 const app = express();
-app.use(express.json());
 
 app.use(express.json());
 app.use(cors());
@@ -14,5 +14,6 @@ app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/login', loginRouter);
 app.use('/products', productRouter);
 app.use('/register', registerRouter);
+app.use('/sales', salesRouter);
 
 module.exports = app;
