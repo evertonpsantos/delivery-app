683b0a2748e68c805c4d844c8ae7b163de9d4457
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2023-03-05 16:38:03 -0300

    fix: arrumando função para não arrays

--- back-end/src/helpers/transformToObject.js
+++ back-end/src/helpers/transformToObject.js
@@ -1,12 +1,17 @@
-const transformToObject = (a, b, quantity) => {
+const userService = require('../services/UserService');
+
+const transformToObject = async (sale, product, quantity) => {
+  const priceProduct = parseFloat(product.price).toFixed(2);
+  const totalPriceSale = parseFloat(sale.totalPrice).toFixed(2).toString().replace('.', ',');
+  const sellerById = await userService.getSellerById(sale.sellerId);
   const obj = {
-    id: a.id,
-    totalPrice: a.totalPrice,
-    saleDate: a.saleDate,
-    status: a.status,
-    name: b.name,
-    price: b.price,
-    urlImage: b.urlImage,
+    id: sale.id,
+    seller: sellerById.name,
+    totalPrice: totalPriceSale,
+    saleDate: sale.saleDate,
+    status: sale.status,
+    name: product.name,
+    price: priceProduct,
     quantity,
   };
   return obj;
