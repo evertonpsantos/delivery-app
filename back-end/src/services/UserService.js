8833ae7d2679fc943d76e9b4732c1c32d9eed6e5
evertonpsantos <evertonpsantos8@gmail.com>
2023-03-06 13:59:44 -0300

    fix: troca validação do login para md5

--- back-end/src/services/UserService.js
+++ back-end/src/services/UserService.js
@@ -1,15 +1,15 @@
-const bcrypt = require('bcrypt');
+const md5 = require('md5');
 const { User } = require('../database/models');
 
 const loginService = async ({ email, password }) => {
-  const userFound = await User.findOne({
+  const { dataValues: userFound } = await User.findOne({
     where: { email },
   });
   
   if (!userFound) throw new Error('User not found');
 
-  if (!bcrypt.compare(password, userFound.password)) {
-    throw new Error('User not found');
+  if (md5(password) !== userFound.password) {
+    throw new Error('Wrong password');
   }
 
   return userFound;
