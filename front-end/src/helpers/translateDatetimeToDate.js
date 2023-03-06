4663d2a2822e835c17436ca920da8c61d803ee10
fernando r <fe.dornellesrocha@outlook.com>
2023-03-06 09:47:41 -0300

    fix: try 1

--- front-end/src/helpers/translateDatetimeToDate.js
+++ front-end/src/helpers/translateDatetimeToDate.js
@@ -3,17 +3,12 @@ function translateDatetimeToDate(arr) {
     const toDate = new Date(e.saleDate);
     const year = toDate.getUTCFullYear();
     const month = toDate.getUTCMonth() + 1;
-    let day = toDate.getUTCDate();
-    if (day.length < 2) {
-      day = `0${day}`;
-    }
-    const dateString = `${month}/${day}/${year}`;
+    const day = toDate.getUTCDate();
+    const dateString = `${month}-${day}-${year}`;
     const newDate = { ...e, saleDate: dateString };
     return newDate;
   });
   return mappedArray;
 }
-
-// coment
 
 export default translateDatetimeToDate;
