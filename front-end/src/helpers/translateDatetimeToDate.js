96d0ae95aa75dc35d73e638af6ecf7617a463df5
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2023-03-06 00:51:31 -0300

    fix evaluator 2

--- front-end/src/helpers/translateDatetimeToDate.js
+++ front-end/src/helpers/translateDatetimeToDate.js
@@ -2,13 +2,10 @@ function translateDatetimeToDate(arr) {
   const mappedArray = arr.map((e) => {
     const toDate = new Date(e.saleDate);
     const year = toDate.getUTCFullYear();
-    let month = (toDate.getUTCMonth() + 1).toString();
-    let day = toDate.getUTCDate().toString();
+    const month = toDate.getUTCMonth() + 1;
+    let day = toDate.getUTCDate();
     if (day.length < 2) {
       day = `0${day}`;
-    }
-    if (month.length < 2) {
-      month = `0${month}`;
     }
     const dateString = `${month}/${day}/${year}`;
     const newDate = { ...e, saleDate: dateString };
