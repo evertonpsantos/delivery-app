4117719773dbc7b7c9d7aa85e5d7ca36ef6a9d30
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2023-03-05 22:49:38 -0300

    trying to pass in github check

--- front-end/src/helpers/translateDatetimeToDate.js
+++ front-end/src/helpers/translateDatetimeToDate.js
@@ -1,10 +1,10 @@
-const NUMBER10 = 10;
-
 function translateDatetimeToDate(arr) {
   const mappedArray = arr.map((e) => {
     const toDate = new Date(e.saleDate);
-    const date = toDate.toLocaleString();
-    const dateString = date.slice(0, NUMBER10);
+    const year = toDate.getUTCFullYear();
+    const month = toDate.getUTCMonth() + 1;
+    const day = toDate.getUTCDate();
+    const dateString = `${month}-${day}-${year}`;
     const newDate = { ...e, saleDate: dateString };
     return newDate;
   });
