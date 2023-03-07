a1eebd6061c5c53e950e0d48bc200a15b26b58ce
evertonpsantos <evertonpsantos8@gmail.com>
2023-03-07 14:51:08 -0300

    feat: adiciona navbar especifica para manager

---
+++ front-end/src/components/NavBarManager.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+// import '../styles/navBarCustomer.css';
+import { Link } from 'react-router-dom';
+
+function NavBarManager() {
+  const [userName, setUserName] = useState('');
+
+  const logout = () => {
+    localStorage.removeItem('user');
+  };
+
+  useEffect(() => {
+    const getUserName = () => {
+      const user = JSON.parse(localStorage.getItem('user')) || '';
+      if (user) {
+        setUserName(user.name);
+      }
+    };
+
+    getUserName();
+  }, []);
+
+  return (
+    <nav className="navbar">
+      <div className="left_side">
+        <div
+          className="navbar_pedidos"
+        >
+          <Link
+            data-testid="customer_products__element-navbar-link-orders"
+            to="/customer/orders"
+          >
+            GERENCIAR USUÁRIOS
+          </Link>
+        </div>
+
+      </div>
+
+      <div className="right_side">
+        <div
+          className="navbar_nome"
+          data-testid="customer_products__element-navbar-user-full-name"
+        >
+          { userName }
+        </div>
+
+        <div className="navbar_sair">
+          <Link
+            to="/login"
+            data-testid="customer_products__element-navbar-link-logout"
+            onClick={ () => logout() }
+          >
+            Sair
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBarManager;
